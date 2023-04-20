655864f383c009b29812b3db9d972d7c93210afd
amarcoe <54554581+amarcoe@users.noreply.github.com>
2023-04-20 16:25:02 -0500

    Salt/hash working, new user/update working

---
+++ backend/database.py
@@ -0,0 +1,72 @@
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from werkzeug.security import check_password_hash
+
+
+db = SQLAlchemy()
+
+
+class Posts(db.Model):
+    __tablename__ = "posts"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=False)
+    # Will get from current_user
+    brewer = db.Column(db.String(80), nullable=False, unique=False)
+    # Will be input by user
+    coarseness = db.Column(db.String(80), nullable=True, unique=False)
+    # Will be chosen by user
+    recipe = db.Column(db.ARRAY(db.String(200)))
+    # Array entered by user
+    roast_level = db.Column(db.String(80), nullable=False, unique=False)
+    # Entered by user
+    bookmarked = db.Column(db.ARRAY(db.String(80)))
+    # Think it will be an array added to with each current_user
+    post_date = db.Column(db.String(80), nullable=False, unique=False)
+    # I'll just use datetime to pass this information in the backend
+
+
+#     Author ID
+# Roaster (optional)
+# Brewer (required)
+# Coarseness (optional)
+# If coarseness, then what grinder did you use?
+# Recipe Make it an array, each step is an index
+# Roast Level
+# List of users who have bookmarked it
+# Date of post
+
+
+class Users(db.Model, UserMixin):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(200), nullable=False, unique=False)
+    brewers = db.Column(db.ARRAY(db.String(80)), nullable=True, unique=False)
+    grinder = db.Column(db.ARRAY(db.String(80)), nullable=True, unique=False)
+    roaster = db.Column(db.String(100), nullable=True, unique=False)
+    # recipes = db.Column(db.ARRAY(db.ForeignKey("posts.id")))
+    # Need to get ForeignKey working, haven't looked into it much
+
+    #     Username
+    # Password
+    # Equipment (optional)
+    # Favorite Roasters (top 3)
+    # Bookmarked recipes
+    # User Recipes
+
+    def authenticate(username, password):
+        user = Users.query.filter_by(username=username).first()
+        print("I'm in authenticate")
+        print(user.password)
+        if user and check_password_hash(user.password, password):
+            print(password)
+            print("Let's fucking go")
+            return user
+        else:
+            return None
+
+
+def create_table(app):
+    with app.app_context():
+        db.create_all()
---
+++ backend/server.py
@@ -0,0 +1,68 @@
+import os
+import flask
+import requests
+from database import create_table, Users, db, Posts
+from datetime import datetime
+from flask_login import LoginManager, login_required, login_user, current_user
+from werkzeug.security import generate_password_hash
+
+app = flask.Flask(__name__)
+app.secret_key = os.getenv("secret_key")
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://amarcoe@localhost:5432/postgres"
+
+login_manager = LoginManager()
+
+db.init_app(app)
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(id):
+    return Users.query.get(id)
+
+
+create_table(app)
+
+
+@app.route("/signup")
+def login():
+    # With password I'll get it form form
+    # Theoretically form_data = flask.requests.form
+    # generate_password_hash(form_data[password])
+    password = "anything"
+    # Just a dummy password to test against, will drop table don't take off points
+    hashed_password = generate_password_hash(password)
+    new_user = Users(
+        username="dummy",
+        password=hashed_password,
+        brewers=["Chemex", "Aeropress", "Clever"],
+        grinder=["Baratza Encore, Fellow Ode"],
+        roaster="Eiland",
+    )
+    db.session.add(new_user)
+    db.session.commit()
+    # Test if character limit is per string or entire list
+    # If it's under given amount type more and try to break it
+
+
+@app.route("/update")
+def update_user():
+    Users.authenticate("dummy", "anything")
+    # Proves salt/hash works
+    user = Users.query.get(1)
+    # Just grabbed a random one to make sure I know how to update
+    if user:
+        # Update the user's information
+        user.username = user.username
+        user.password = user.password
+        user.brewers = user.brewers + ["Moka Pot"]
+        user.grinder = user.brewers + ["Blade grinder"]
+        user.roaster = "Palace"
+        db.session.commit()
+        db.session.refresh(user)
+        flask.flash("User information updated!")
+    else:
+        print("Nothing")
+
+
+app.run(debug=True)
