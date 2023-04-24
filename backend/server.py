6891188cb7a412c8dc201a60b9c8325d12f010de
amarcoe <54554581+amarcoe@users.noreply.github.com>
2023-04-24 17:23:38 -0500

    Have to stash these changes

--- backend/server.py
+++ backend/server.py
@@ -2,11 +2,14 @@ import os
 import flask
 import requests
 import datetime
-from backend.database import create_table, Users, db, Posts
+from database import create_table, Users, db, Posts
 from flask_login import LoginManager, login_required, login_user, current_user
 from werkzeug.security import generate_password_hash
+from flask import request, make_response
+from flask_cors import CORS
 
 app = flask.Flask(__name__)
+CORS(app)
 app.secret_key = os.getenv("secret_key")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 
@@ -24,16 +27,12 @@ def load_user(id):
 create_table(app)
 
 
-@app.route("/")
-def whatever():
-    # Will be home.jsx
-    return flask.redirect(flask.url_for("handle_post"))
-
-
 @app.route("/login")
 def handle_login():
     form_data = flask.request.form
-    user = Users.authenticate("dummy", "anything")
+    username = form_data["username"]
+    password = form_data["password"]
+    user = Users.authenticate(username, password)
     # Will take username and password from form_data and run them through authenticate
     if user:
         login_user(user)
@@ -42,22 +41,38 @@ def handle_login():
         return "Information was incorrect"
 
 
-@app.route("/signup")
+@app.route("/signup", methods=["POST"])
 def handle_signup():
     # Will get information from signup.jsx
     form_data = flask.request.form
     username = request.form["username"]
-    # hashed_password = generate_password_hash(form_data["password"])
+    print(form_data)
+    hashed_password = generate_password_hash(form_data["password"])
     new_user = Users(
-        username=request.form["username"],
-        password=request.form["password"],
-        brewers=request.form["brewers"],
-        grinder=request.form["grinders"],
-        roaster=request.form["roasters"],
+        username=form_data["username"],
+        password=hashed_password,
+        brewers=form_data["brewers"],
+        grinder=form_data["grinder"],
+        # roaster=form_data["roaster"],
     )
     db.session.add(new_user)
     db.session.commit()
-    return brewers
+
+
+@app.route("/post", method=["POST"])
+def handle_post():
+    form_data = flask.request.form
+    new_post = Posts(
+        username=form_data["username"],
+        brewer=form_data["brewer"],
+        coarseness=form_data["coarseness"],
+        recipe=["Array given by form_data"],
+        roast_level="Light",
+        post_date=now,
+    )
+    db.session.add(new_post)
+    db.session.commit()
+    return "New post created"
 
 
 @app.route("/update_user")
@@ -85,25 +100,6 @@ def handle_post_update():
     form_data = flask.request.form
     post = Posts.query.get(1)
     return "Working on it"
-
-
-@app.route("/post")
-def handle_post():
-    form_data = flask.request.form
-    now = datetime.datetime.now()
-    print("I am now")
-    print(now)
-    new_post = Posts(
-        username="Marty",
-        brewer="Chemex",
-        coarseness="26",
-        recipe=["Array given by form_data"],
-        roast_level="Light",
-        post_date=now,
-    )
-    db.session.add(new_post)
-    db.session.commit()
-    return "New post created"
 
 
 # if __name__ == "__main__":
