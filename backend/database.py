45bf8e92d1b85017a46473c21eea2689cd5d24f3
Jonathan Humphrey <humphreyj1999@gmail.com>
2023-04-25 12:55:03 -0500

    trying to merge

--- backend/database.py
+++ backend/database.py
@@ -11,15 +11,10 @@ class Posts(db.Model):
     __tablename__ = "posts"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), nullable=False, unique=False)
-    # Will get from current_user
     brewer = db.Column(db.String(80), nullable=False, unique=False)
-    # Will be input by user
     coarseness = db.Column(db.String(80), nullable=True, unique=False)
-    # Will be chosen by user
-    recipe = db.Column(db.ARRAY(db.String(200)))
-    # Array entered by user
+    recipe = db.Column(db.ARRAY(db.String(1000)), nullable=True, unique=False)
     roast_level = db.Column(db.String(80), nullable=False, unique=False)
-    # Entered by user
     bookmarked = db.Column(db.ARRAY(db.String(80)))
     # Think it will be an array added to with each current_user
     post_date = db.Column(db.String(80), nullable=False, unique=False)
@@ -46,25 +41,18 @@ class Users(db.Model, UserMixin):
     brewers = db.Column(db.ARRAY(db.String(80)), nullable=True, unique=False)
     grinder = db.Column(db.ARRAY(db.String(80)), nullable=True, unique=False)
     roaster = db.Column(db.String(100), nullable=True, unique=False)
-    # bookmarked_recipes = db.Column(db.ARRAY(db.ForeignKey("posts.id")))
-    # Need to get ForeignKey working, haven't looked into it much
-    user_recipes = db.Column(db.ARRAY(db.String(10000)), nullable=True, unique=False)
-    # Need to test this one
+    bookmarked_recipes = db.Column(db.ARRAY(db.Integer), nullable=True, unique=False)
+    user_recipes = db.Column(db.ARRAY(db.Integer), nullable=True, unique=False)
 
-    #     Username
-    # Password
-    # Equipment (optional)
-    # Favorite Roasters (top 3)
-    # Bookmarked recipes
-    # User Recipes
+    # user = Users.query.filter_by(username=username).first()
+    # bookmarked_posts = Posts.query.filter(Posts.id.in_(user.bookmarked_recipes)).all()
+    # user_posts = Posts.query.filter(Posts.id.in_(user.user_recipes)).all()
+    # I'm not sure when bookmarked_recipes will be updated and I wrote when user_recipes will be
+    # The lines above will query posts by what the recipes columns are storing
 
     def authenticate(username, password):
         user = Users.query.filter_by(username=username).first()
-        print("I'm in authenticate")
-        print(user.password)
         if user and check_password_hash(user.password, password):
-            print(password)
-            print("Let's fucking go")
             return user
         else:
             return None
